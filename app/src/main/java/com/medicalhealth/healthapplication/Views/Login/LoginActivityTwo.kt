4d8078ccc22f3dda3e945ca54a0fbb9769f403b2
1702 <abin.prasad@trenser.com>
2025-08-25 09:58:07 +0530

    Login 1 finished

---
+++ app/src/main/java/com/medicalhealth/healthapplication/Views/Login/LoginActivityTwo.kt
@@ -0,0 +1,17 @@
+package com.medicalhealth.healthapplication.Views.Login
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.medicalhealth.healthapplication.R
+
+class LoginActivityTwo : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login_two)
+
+    }
+}
