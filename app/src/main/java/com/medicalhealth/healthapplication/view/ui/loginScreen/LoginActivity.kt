55fe297e3fb18b6267a9827a2e1324a3835f6292
Mathews Jose <mathews.jose@trenser.com>
2025-08-25 18:40:57 +0530

    #387 Completed Profile Part Design

--- app/src/main/java/com/medicalhealth/healthapplication/view/ui/loginScreen/LoginActivity.kt
+++ app/src/main/java/com/medicalhealth/healthapplication/view/ui/loginScreen/LoginActivity.kt
@@ -1,15 +1,28 @@
 package com.medicalhealth.healthapplication.view.ui.loginScreen
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.medicalhealth.healthapplication.R
+import com.medicalhealth.healthapplication.databinding.ActivityLoginBinding
+import com.medicalhealth.healthapplication.view.MainActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.login.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
     }
 }
