716ff6d1f9c7a818b10f39d77d12593626429c24
1702 <abin.prasad@trenser.com>
2025-08-25 10:46:46 +0530

    Login 1 & 2 finishd

--- app/src/main/java/com/medicalhealth/healthapplication/Views/Login/Login.kt
+++ app/src/main/java/com/medicalhealth/healthapplication/Views/Login/Login.kt
@@ -1,6 +1,8 @@
 package com.medicalhealth.healthapplication.Views.Login
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,12 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+        val button =findViewById<Button>(R.id.login)
+        button.setOnClickListener {
+            val intent= Intent(this, LoginActivityTwo::class.java)
+            startActivity(intent)
         }
+
     }
 }
