4fe9ecd4c80c353e3a9c3c38505d44f10d16ca62
Mathews Jose <mathews.jose@trenser.com>
2025-08-27 16:19:20 +0530

    #393 implement Home Screen Activity with doctor schedule RecyclerView

--- app/src/main/java/com/medicalhealth/healthapplication/view/MainActivity.kt
+++ app/src/main/java/com/medicalhealth/healthapplication/view/MainActivity.kt
@@ -2,15 +2,51 @@ package com.medicalhealth.healthapplication.view
 
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.medicalhealth.healthapplication.R
+import com.medicalhealth.healthapplication.databinding.ActivityMainBinding
+import com.medicalhealth.healthapplication.view.adapter.DateAdapter
+import com.medicalhealth.healthapplication.view.adapter.DoctorAdapter
+import com.medicalhealth.healthapplication.view.adapter.ScheduleAdapter
+import com.medicalhealth.healthapplication.viewModel.MainViewModel
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var mainBinding: ActivityMainBinding
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
 
+        val dateAdapter = DateAdapter(viewModel.dates.value!!) { selectedDate ->
+            viewModel.selectDate(selectedDate)
+        }
 
+        with(mainBinding){
+            dateRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity, LinearLayoutManager.HORIZONTAL, false)
+            dateRecyclerView.adapter = dateAdapter
+
+            viewModel.dates.observe(this@MainActivity){updatedDates->
+                dateAdapter.notifyDataSetChanged()
+            }
+
+            scheduleRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            viewModel.appointment.observe(this@MainActivity){appointmentsList ->
+                val appointmentAdapter = ScheduleAdapter(appointmentsList)
+                scheduleRecyclerView.adapter = appointmentAdapter
+            }
+
+            doctorRecyclerView.layoutManager = LinearLayoutManager(this@MainActivity)
+            viewModel.doctors.observe(this@MainActivity){ doctorsList ->
+                val adapter = DoctorAdapter(doctorsList) {doctor ->
+                    viewModel.toggleFavoriteStatus(doctor.id)
+                }
+                doctorRecyclerView.adapter = adapter
+            }
+        }
     }
 }
---
+++ app/src/main/java/com/medicalhealth/healthapplication/viewModel/MainViewModel.kt
@@ -0,0 +1,60 @@
+package com.medicalhealth.healthapplication.viewModel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.medicalhealth.healthapplication.R
+import com.medicalhealth.healthapplication.model.data.Appointment
+import com.medicalhealth.healthapplication.model.data.Date
+import com.medicalhealth.healthapplication.model.data.Doctor
+
+class MainViewModel: ViewModel() {
+    private val _dates = MutableLiveData<List<Date>>()
+    val dates: LiveData<List<Date>> get() = _dates
+
+    private val _appointments = MutableLiveData<List<Appointment>>()
+    val appointment: LiveData<List<Appointment>> get() = _appointments
+
+    private val _doctors = MutableLiveData<List<Doctor>>()
+    val doctors: LiveData<List<Doctor>> get() = _doctors
+
+    init {
+        _dates.value = listOf(
+            Date("9", "MON", false),
+            Date("10", "TUE", false),
+            Date("11", "WED", true),
+            Date("12", "THU", false),
+            Date("13", "FRI", true),
+            Date("14", "SAT", true),
+        )
+        _appointments.value = listOf(
+            Appointment("1", "10 AM", "Dr. Olivia Turner, M.D.", "Treatment and prevention of skin and photodermatitis.")
+        )
+
+        _doctors.value = listOf(
+            Doctor("1", R.drawable.doctor_image_one, "Dr. Olivia Turner, M.D.", "Dermato-Endocrinology", 5.0, 60, true),
+            Doctor("2", R.drawable.doctor_image_two, "Dr. Alexander Bennett, Ph.D.", "Dermato-Genetics", 4.5, 40, false),
+            Doctor("3", R.drawable.doctor_image_three, "Dr. Sophia Martinez, Ph.D.", "Cosmetic Bioengineering", 5.0, 150, false),
+            Doctor("4", R.drawable.doctor_image_four, "Dr. Michael Davidson, M.D.", "Nano-Dermatology", 4.8, 90, true),
+        )
+    }
+
+    fun selectDate(selectedDate: Date){
+        val updatedList = _dates.value?.map { date ->
+            date.isSelected = (date == selectedDate)
+            date
+        }
+        _dates.value = updatedList
+    }
+
+    fun toggleFavoriteStatus(doctorId: String){
+        val updatedList = _doctors.value?.map { doctor ->
+            if(doctor.id == doctorId){
+                doctor.copy(isFavorite = !doctor.isFavorite)
+            }else{
+                doctor
+            }
+        }
+        _doctors.value = updatedList
+    }
+}
