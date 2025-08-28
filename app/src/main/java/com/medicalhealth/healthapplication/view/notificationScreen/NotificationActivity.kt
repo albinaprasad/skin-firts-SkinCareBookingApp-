32cd0093ff0d0a17286049454c770f964f47a4d7
1702 <abin.prasad@trenser.com>
2025-08-28 11:31:50 +0530

    #381 ui  for notification started

---
+++ app/src/main/java/com/medicalhealth/healthapplication/view/notificationScreen/NotificationActivity.kt
@@ -0,0 +1,17 @@
+package com.medicalhealth.healthapplication.view.notificationScreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.medicalhealth.healthapplication.R
+
+class NotificationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_notification)
+
+    }
+}
