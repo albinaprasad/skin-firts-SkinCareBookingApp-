6c02c9f96dd927a5b56e2aa6b46e5bf75fdad63b
Meenakshy <meenakshy.santhosh@trenser.com>
2025-09-01 14:48:40 +0530

    #387 Updated the code by adding viewbinding

--- app/src/main/java/com/medicalhealth/healthapplication/view/DoctorsInfoActivity.kt
+++ app/src/main/java/com/medicalhealth/healthapplication/view/DoctorsInfoActivity.kt
@@ -6,12 +6,15 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.medicalhealth.healthapplication.R
+import com.medicalhealth.healthapplication.databinding.ActivityDoctorsInfoBinding
 
 class DoctorsInfoActivity : AppCompatActivity() {
+    lateinit var binding: ActivityDoctorsInfoBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_doctors_info)
+        binding = ActivityDoctorsInfoBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
