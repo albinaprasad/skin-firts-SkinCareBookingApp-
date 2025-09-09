127b23647145339c9c23f730cd012da29a9f6a4a
1702 <abin.prasad@trenser.com>
2025-09-09 16:10:09 +0530

    #363 ui started

---
+++ app/src/main/java/com/medicalhealth/healthapplication/view/passwordManagerScreen/PasswordManagerActivity.kt
@@ -0,0 +1,17 @@
+package com.medicalhealth.healthapplication.view.passwordManagerScreen
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.medicalhealth.healthapplication.R
+
+class PasswordManagerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_password_manager)
+
+    }
+}
