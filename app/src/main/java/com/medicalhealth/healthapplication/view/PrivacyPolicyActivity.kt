927e849cd26a1b9ebd815a97dca5a77fba56675d
Meenakshy <meenakshy.santhosh@trenser.com>
2025-09-12 11:51:14 +0530

    #365 Completed Privacy Policy UI

--- app/src/main/java/com/medicalhealth/healthapplication/view/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/medicalhealth/healthapplication/view/PrivacyPolicyActivity.kt
@@ -1,5 +1,6 @@
 package com.medicalhealth.healthapplication.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import android.webkit.WebView
@@ -8,24 +9,29 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.medicalhealth.healthapplication.R
+import com.medicalhealth.healthapplication.databinding.ActivityPrivacyPolicyBinding
 
 class PrivacyPolicyActivity : BaseActivity() {
+    private lateinit var binding:ActivityPrivacyPolicyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_privacy_policy)
+        binding = ActivityPrivacyPolicyBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val webView: WebView = findViewById(R.id.privacyPolicyWebView)
+        binding.backbtnPrivacyPolicy.setOnClickListener {
+            finish()
+        }
 
         // Set a WebViewClient to keep the navigation within your app
-        webView.webViewClient = WebViewClient()
+        binding.privacyPolicyWebView.webViewClient = WebViewClient()
 
 
-        webView.loadUrl("file:///android_asset/Privacy_Policy/terms_and_condition.html")
+        binding.privacyPolicyWebView.loadUrl("file:///android_asset/Privacy_Policy/terms_and_condition.html")
     }
 
     override fun onBackPressed() {
-        if (findViewById<WebView>(R.id.privacyPolicyWebView).canGoBack()) {
-            findViewById<WebView>(R.id.privacyPolicyWebView).goBack()
+        if (binding.privacyPolicyWebView.canGoBack()) {
+            binding.privacyPolicyWebView.goBack()
         } else {
             super.onBackPressed()
         }
