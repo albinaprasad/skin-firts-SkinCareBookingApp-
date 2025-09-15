930f9fe3064bbae8a2b3abde2fee1775d2969745
Meenakshy <meenakshy.santhosh@trenser.com>
2025-09-15 13:59:37 +0530

    #365 Resolved Comments

--- app/src/main/java/com/medicalhealth/healthapplication/view/PrivacyPolicyActivity.kt
+++ app/src/main/java/com/medicalhealth/healthapplication/view/PrivacyPolicyActivity.kt
@@ -1,32 +1,27 @@
 package com.medicalhealth.healthapplication.view
 
-import android.content.Intent
+
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import android.webkit.WebView
 import android.webkit.WebViewClient
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.medicalhealth.healthapplication.R
 import com.medicalhealth.healthapplication.databinding.ActivityPrivacyPolicyBinding
 
 class PrivacyPolicyActivity : BaseActivity() {
-    private lateinit var binding:ActivityPrivacyPolicyBinding
+    private lateinit var binding: ActivityPrivacyPolicyBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPrivacyPolicyBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.backbtnPrivacyPolicy.setOnClickListener {
-            finish()
-        }
-
-        // Set a WebViewClient to keep the navigation within your app
         binding.privacyPolicyWebView.webViewClient = WebViewClient()
-
-
         binding.privacyPolicyWebView.loadUrl("file:///android_asset/Privacy_Policy/terms_and_condition.html")
+        setUpOnListener()
+    }
+
+    private fun setUpOnListener() {
+        with(binding) {
+            backbtnPrivacyPolicy.setOnClickListener {
+                finish()
+            }
+        }
     }
 
     override fun onBackPressed() {
